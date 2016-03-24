fd84642978b9d490b6ef3b9f6ca5260d30ea2b65
Martijn Nahumury <Martijn@MacBook-Pro-van-Martijn.local>
2016-03-24 14:37:08 -0500

    set up first dice

---
+++ DieLabel.h
@@ -0,0 +1,18 @@
+//
+//  DieLabel.h
+//  Farkle
+//
+//  Created by Martijn Nahumury on 24/03/16.
+//  Copyright © 2016 MFN Advies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DieLabel : UILabel
+
+
+-(instancetype) initWithName:(NSString *)name withDescription:(NSString *)
+   
+ -(instancetype) initWithName:(NSString *)name withDescription:(NSString *)dinosaurDescription withImage:(UIImage *)dinosaurImage andAttributes:(NSArray *)attributes {
+
+@end
---
+++ DieLabel.m
@@ -0,0 +1,21 @@
+//
+//  DieLabel.m
+//  Farkle
+//
+//  Created by Martijn Nahumury on 24/03/16.
+//  Copyright © 2016 MFN Advies. All rights reserved.
+//
+
+#import "DieLabel.h"
+
+@implementation DieLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
