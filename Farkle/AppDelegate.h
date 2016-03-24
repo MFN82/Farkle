09391b468075ce901da2f362264b80b1ab07c1db
Martijn Nahumury <martijnnahumury@yahoo.com>
2016-03-24 14:13:20 -0500

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by Martijn Nahumury on 24/03/16.
+//  Copyright © 2016 MFN Advies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Farkle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by Martijn Nahumury on 24/03/16.
+//  Copyright © 2016 MFN Advies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
